5defbce3620b2e2450eca70b0ccac018d74f5f38
jose.munin <jose.munin@campusdual.com>
2023-07-05 10:03:44 +0200

    Se corrige la salida de la parte decimal como indica el tutor.

--- src/main/java/com/example/ejercicio_05_1/Main.java
+++ src/main/java/com/example/ejercicio_05_1/Main.java
@@ -2,30 +2,28 @@ package com.example.ejercicio_05_1;
 
 import com.example.utils.Input;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+// 5. Dado un numero decimal, devolver por separado la parte entera y la parte decimal.
 public class Main {
     public static void main(String[] args) {
-        String numero = Input.string("Introduce un número: ").replace(',','.');
+        String numero = Input.string("Introduce un número: ").replace(',', '.');
         double parse_numero;
         try {
             parse_numero = Double.parseDouble(numero);
             String parteEntera = getParteEntera(numero);
             String parteDecimal = getParteDecimal(numero);
-            System.out.printf("La parte entera de '%s' es %s y la parte decimal %s.\n", numero, parteEntera, parteDecimal);
+            System.out.printf("La parte entera de '%s' es '%s' y la parte decimal '0.%s'.\n", numero, parteEntera, parteDecimal);
         } catch (Exception e) {
             System.out.printf("'%s' no es un número válido.", numero);
         }
     }
 
-
     private static String getParteEntera(String numero) {
         int index = numero.indexOf('.') == -1 ? numero.length() : numero.indexOf('.');
-        return numero.substring(0,index)  ;
+        return numero.substring(0, index);
     }
 
     private static String getParteDecimal(String numero) {
         int index = numero.indexOf('.') == -1 ? numero.length() : numero.indexOf('.') + 1;
-        return numero.substring(index);
+        return (numero.substring(index).length() == 0 ? "0" : numero.substring(index));
     }
 }
