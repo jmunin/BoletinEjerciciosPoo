54ac97e88d97a4a8db23df2e42502c4249e10f37
jose.munin <jose.munin@campusdual.com>
2023-07-05 08:53:16 +0200

    Añadida corrección sugerida por el tutor: En el ejercicio 2 solo se deberían mostrar los número primos.

--- src/main/java/com/example/ejercicio_02/Main.java
+++ src/main/java/com/example/ejercicio_02/Main.java
@@ -2,8 +2,14 @@ package com.example.ejercicio_02;
 
 import com.example.utils.Input;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+2. Pedir un numero al usuario. Mostrar si el número es múltiplo de cada uno de los
+        numero primos existentes entre 1 y 10;
+*/
+
 public class Main {
     public static void main(String[] args) {
         String numero = Input.string("Introduce un número entero: ");
@@ -11,11 +17,27 @@ public class Main {
         try {
             parse_numero = Integer.parseInt(numero);
             for (int i = 1; i < 11; i++) {
-                if (parse_numero % i == 0)
-                    System.out.printf("El número %d es múltiplo de %d.\n", parse_numero, i);
+                if (esPrimo(i)) {
+                    if (parse_numero % i == 0)
+                        System.out.printf("El número %d es múltiplo de %d.\n", parse_numero, i);
+                }
             }
         } catch (Exception e) {
             System.out.printf("'%s' no es un número entero válido.", numero);
         }
+    }
+
+    //El número 1 no es un número primo porque por definición, un número primo es un número
+    //entero mayor que 1 que solo tiene como divisores positivos a sí mismo y a la unidad.
+    // El número 1 no cumple con esta definición ya que solo tiene un divisor.
+    public static boolean esPrimo(int numero) {
+        int contador = 2;
+        boolean primo = numero != 1;
+        while ((primo) && (contador != numero)) {
+            if (numero % contador == 0)
+                primo = false;
+            contador++;
+        }
+        return primo;
     }
 }
